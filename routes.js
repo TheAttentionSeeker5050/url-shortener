ee7634199e795f07cd2db551e7aca28b8888d17e
Nicolas@ <nicolas.castellano.6@gmail.com>
2022-09-16 20:10:36 -0300

    steps before adding login and register functionalities

--- routes.js
+++ routes.js
@@ -3,6 +3,24 @@ module.exports = function (app, opts) {
   // Setup routes, middleware, and handlers
   app.get('/', (req, res) => {
     res.locals.name = 'url-shortener'
-    res.render('index')
+    res.locals.page_name = "home"
+    res.render('main')
   })
+
+  app.get('/login', (req, res) => {
+    res.locals.page_name = "login"
+    res.render('main')
+  })
+
+  app.get('/register', (req, res) => {
+    res.locals.page_name = "register"
+    res.render('main')
+  })
+
+  app.get('/my-urls', (req, res) => {
+    res.locals.page_name = "my-urls"
+    res.render('main')
+  })
+
 }
+
